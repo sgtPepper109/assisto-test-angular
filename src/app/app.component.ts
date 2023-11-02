458a34f602287065204b4bb60c40f8e461c6027d
sgtPepper109 <a.r.shaikh1940@gmail.com>
2023-11-03 01:06:39 +0530

    assisto-angular-json-table-test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Service } from './service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'assisto-test';
+export class AppComponent implements OnInit {
+	constructor(private service: Service) { }
+	masterData: Array<Object> = []
+	maxColumnSize: number = 0;
+	tableColumns: Array<string> = ['done', 'name', 'due_date', 'rating', 'recurrence', 'recurrence_interval', 'description']
+	title = 'assisto-test';
+
+	// refresh button
+	refresh(): void {
+		this.service.getJsonData().subscribe({
+			next: (response: Object) => {
+				this.masterData = Object.values(response)
+			},
+			error: (error: HttpErrorResponse) => {
+				console.log(error)
+				alert(error.message)
+			}
+		})
+	}
+
+	ngOnInit(): void {
+		this.refresh()
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		FormsModule,
+		ReactiveFormsModule,
+		BrowserAnimationsModule,
+		HttpClientModule,
+		MatTableModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
